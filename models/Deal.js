dfb705f6912245b4ef82cfacdad6a9a02d1028b0
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-09-18 15:12:35 -0300

    feat: Deal now using correct collection name

--- models/Deal.js
+++ models/Deal.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const dealSchema = new mongoose.Schema({
-  house: { type: String, required: true },
-  grade: { type: Number, default: 0 },
-  description: String,
-  soldOut: { type: Boolean, required: true },
-  createdDate: { type: String, required: true },
-});
+const dealSchema = new mongoose.Schema(
+  {
+    house: { type: String, required: true },
+    grade: { type: Number, default: 0 },
+    description: String,
+    soldOut: { type: Boolean, required: true },
+    createdDate: { type: String, required: true },
+  },
+  { collection: "deals" }
+);
 
-module.exports = mongoose.model("Deal", dealSchema, "deals");
+module.exports = mongoose.model("Deal", dealSchema);
