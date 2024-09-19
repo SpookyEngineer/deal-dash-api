2a92560f6f703ccf9e1043439e984c55b9d9dad8
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-09-19 16:56:07 -0300

    feat: preparing application for vercel deployment

--- api/index.js
+++ api/index.js
@@ -23,3 +23,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// index.js (or your entry point file)
+module.exports = (req, res) => {
+  res.status(200).send("Betpass assessment backend");
+};
