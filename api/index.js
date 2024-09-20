bc04170840e86201dbb8ef72d5fdade2a7f0de56
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-09-19 21:31:37 -0300

    feat: configured CORS

--- api/index.js
+++ api/index.js
@@ -16,7 +16,13 @@ connectDB();
 // Middleware
 app.use(express.json()); // Parse JSON requests
 
-app.use(cors()); // CORS enabled
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: false,
+  })
+); // CORS enabled without authentication since not meant for production
 
 // Routes
 app.get("/", (req, res) =>
